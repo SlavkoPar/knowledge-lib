aa3919fd2698ffeacf512c7e65c26ad7a593feb8
SlavkoPar <slavko.parezanin@gmail.com>
2025-12-08 16:08:58 +0100

    navig

--- src/ChatBotDlgNavigator.tsx
+++ src/ChatBotDlgNavigator.tsx
@@ -1,4 +1,4 @@
-import { useContext, type ReactNode, forwardRef, useImperativeHandle, useState, useCallback } from 'react';
+import { useContext, type ReactNode, forwardRef, useImperativeHandle, useState } from 'react';
 import { Accordion, AccordionContext, Button, Card, useAccordionButton } from "react-bootstrap";
 //import { useNavigate } from "react-router-dom";
 
